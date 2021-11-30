4c134a8117e60fcb403809c7bdc8bee7059abd7d
吴渊 <wrzmonkey@gmail.com>
2021-11-30 11:01:20 +0800

    fix

--- packages/utils/src/react.ts
+++ packages/utils/src/react.ts
@@ -1,16 +1,14 @@
 export const isClassComponent = (component: any) => {
   return (
     typeof component === 'function' &&
-    Boolean(component.prototype.isReactComponent)
+    Boolean(component.prototype?.isReactComponent)
   );
 };
 
 export const isFunctionalComponent = (Component: any) => {
   return (
     typeof Component === 'function' && // can be various things
-    !(
-      (Component.prototype && Component.prototype.isReactComponent) // native arrows don't have prototypes
-    )
+    !Component.prototype?.isReactComponent // native arrows don't have prototypes
   );
 };
 
